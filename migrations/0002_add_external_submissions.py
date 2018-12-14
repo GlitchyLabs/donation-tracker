732448d966a89517835957ca97841e17133135aa
Dennis Bartlett <bartlett.dc.1@gmail.com>
2018-12-14 05:12:19 -0500

    Update 0002_add_external_submissions.py

--- migrations/0002_add_external_submissions.py
+++ migrations/0002_add_external_submissions.py
@@ -19,6 +19,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(unique=True, max_length=64)),
+                ('botname', models.CharField(unique=False, blank=True, max_length=64)),
                 ('stream', models.URLField(max_length=128)),
                 ('twitter', models.SlugField(max_length=15)),
                 ('youtube', models.SlugField(max_length=20)),
